cad92474758f483a13b09131f90f02274012a868
Hubert Lefevre <hubelefevre@gmail.com>
2026-02-25 12:59:40 +0100

    Fix example

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 
-use brio_bluetooth::{BrioSmartTech, Color};
+use brio_smart_tech::{BrioSmartTech, Color};
 use btleplug::{api::Manager as _, platform::Manager};
 use strum::IntoEnumIterator;
 use tokio::time::{Duration, sleep};
@@ -15,31 +15,30 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     println!("Searching for train");
     let train = BrioSmartTech::new(central)
-        .await?
-        .expect("device not found");
+        .await?;
 
     println!("Sending different colors");
     for c in Color::iter() {
         println!("Color {c:?}");
         for i in 0..16 {
-            train.set_color(c, i).await?;
+            train.lock().await.set_color(c, i).await?;
             sleep(Duration::from_millis(100)).await;
         }
     }
 
-    train.set_color(Color::White, 15).await?;
+    train.lock().await.set_color(Color::White, 15).await?;
     sleep(Duration::from_millis(300)).await;
 
     println!("Forward");
-    train.forward(7).await?;
+    train.lock().await.forward(7).await?;
     sleep(Duration::from_secs(10)).await;
 
     println!("Backward");
-    train.backward(7).await?;
+    train.lock().await.backward(7).await?;
     sleep(Duration::from_secs(1)).await;
 
     println!("Stop");
-    train.stop().await?;
+    train.lock().await.stop().await?;
     sleep(Duration::from_millis(300)).await;
 
     Ok(())
